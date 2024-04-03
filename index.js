46b7dc61615e02acf5975474901586ae997deeac
Tran-Anh-Tuan11 <trantuan1112t@gmail.com>
2024-04-03 10:56:24 +0700

    update morgan, nodemon

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 8000
 
+
+app.use(morgan('combined'))
 app.get('/tin', (req, res) => {
-    var a =1;
-    var b = 2;  
-    var c = a + b;
   res.send('Hello World!')
 })
 
