307a3d34a784ce473b03af390f83dfee4ec3b208
Tran-Anh-Tuan11 <trantuan1112t@gmail.com>
2024-04-09 16:53:06 +0700

    update index, home and config handlebars

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 var path = require('path');
 const express = require('express')
 const morgan = require('morgan')
-const handlebars = require('express-handlebars')
+const {engine} = require('express-handlebars')
 const app = express()
 const port = 8000
 
@@ -9,13 +9,17 @@ const port = 8000
 app.use(morgan('combined'))
 
 //Template engine 
-app.engine('handlebars', handlebars)
+app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home')
 })
+
+app.get('/news', (req, res) => {
+    res.render('news')
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
