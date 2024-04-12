1fa61e1905b0367f6b58dff7c66d99e391fb2ad2
Tran-Anh-Tuan11 <trantuan1112t@gmail.com>
2024-04-12 14:41:13 +0700

    sass

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ const {engine} = require('express-handlebars')
 const app = express()
 const port = 8000
 
+app.use(express.static(path.join(__dirname,'public')))
 //HTTP logger
 app.use(morgan('combined'))
 
 //Template engine 
-app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
+app.engine('.hbs', engine(
+   {
+    extname: '.hbs'
+   }
+))
+app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
