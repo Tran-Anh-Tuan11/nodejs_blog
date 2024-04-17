c48849f22850680c03a38beb07d08f35d5c59149
Tran-Anh-Tuan11 <trantuan1112t@gmail.com>
2024-04-17 09:14:56 +0700

    add search and post medthod

--- src/index.js
+++ src/index.js
@@ -1,31 +1,47 @@
 var path = require('path');
 const express = require('express')
 const morgan = require('morgan')
-const {engine} = require('express-handlebars')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 8000
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 //HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 //Template engine 
 app.engine('.hbs', engine(
-   {
-    extname: '.hbs'
-   }
+    {
+        extname: '.hbs'
+    }
 ))
+
+app.use(express.urlencoded(
+    {
+        extended: true
+    }
+))
+app.use(express.json())
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
-  res.render('home')
+    res.render('home')
 })
 
 app.get('/news', (req, res) => {
     res.render('news')
-  })
+})
+
+app.get('/search', (req, res) => {
+    res.render('search')
+})
+
+app.post('/search', (req, res) => {
+    console.log(req.body);
+    res.render('search')
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
