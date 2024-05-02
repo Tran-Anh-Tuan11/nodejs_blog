2634712c269430e502d55df3164e0c07542553d7
Tran-Anh-Tuan11 <trantuan1112t@gmail.com>
2024-05-02 22:48:14 +0700

    add moongoose

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,8 +1,17 @@
+const Course = require('../models/Course')
+
 class SiteController {
 
     //[GET] /
-    index(req,res) {
-        res.render('home')
+    async index(req,res) {
+        try {
+            const courses = await Course.find({});
+            res.json(courses)
+            res.render("home")
+        } catch (error) {
+            res.status(400).json({eror:"Error"})
+        }
+        
     }
 
      //[GET] /
--- src/index.js
+++ src/index.js
@@ -2,21 +2,27 @@ var path = require('path');
 const express = require('express')
 const morgan = require('morgan')
 const { engine } = require('express-handlebars')
+const db = require('./config/db')
+
+//connect to db
+db.connect();
+
 const app = express()
 const port = 8000
 
 const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
 //HTTP logger
 // app.use(morgan('combined'))
-
+ 
 //Template engine 
-app.engine('.hbs', engine(
+app.engine('.hbs', engine( 
     {
         extname: '.hbs'
     }
 ))
-
+  
 app.use(express.urlencoded(
     {
         extended: true
