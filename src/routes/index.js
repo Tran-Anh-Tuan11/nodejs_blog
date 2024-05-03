78461dffc03df5d67cc9810d51ebdd7b36ada1a5
Tran-Anh-Tuan11 <trantuan1112t@gmail.com>
2024-05-03 17:54:59 +0700

    detail courses

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 const newsRouter = require('./news')
 const siteRouter = require('./site')
+const coursesRouter = require('./courses')
 function route(app) {
     app.use('/news', newsRouter)
-
-   app.use('/', siteRouter)
-    
+    app.use('/courses', coursesRouter)
+    app.use('/', siteRouter)
     
  }
 module.exports = route;
